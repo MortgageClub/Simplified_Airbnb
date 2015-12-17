1cbf92c047d762527cd3e071123c56a677e584bc
TangNV <nvtang.khtn@gmail.com>
2015-12-17 09:17:58 +0700

    #3 transactional emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -35,7 +35,16 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    enable_starttls_auto: true,
+    user_name: "tang@mortgageclub.co",
+    password: "NC09Iv4kBPR7hMGGQWNa6w",
+    authentication: "login"
+  }
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
