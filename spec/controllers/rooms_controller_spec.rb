da132d91e041d608dfb44c3d49253dea93a2ba89
TangNV <nvtang.khtn@gmail.com>
2015-12-18 09:29:21 +0700

    fix rubocop

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe RoomsController, :type => :controller do
-
+RSpec.describe RoomsController, type: :controller do
   describe "GET index" do
     it "returns http success" do
       get :index
@@ -43,5 +42,4 @@ RSpec.describe RoomsController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
