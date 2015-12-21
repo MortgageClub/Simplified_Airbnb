fa4c1833833847ff6ad7fc83fc745949752024a1
TangNV <nvtang.khtn@gmail.com>
2015-12-21 10:35:53 +0700

    #17 your trips

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -24,6 +24,10 @@ class ReservationsController < ApplicationController
     redirect_to @reservation.room, notice: "Your reservation has been created..."
   end
 
+  def your_trips
+    @trips = current_user.reservations
+  end
+
   private
 
   def conflict?(start_date, end_date)
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
 
   get '/preload' => 'reservations#preload'
   get '/preview' => 'reservations#preview'
+  get '/your_trips' => 'reservations#your_trips'
 end
