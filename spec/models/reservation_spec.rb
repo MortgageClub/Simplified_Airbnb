8b906e2615091b9afee99a16ffbfe837383a0aa9
TangNV <nvtang.khtn@gmail.com>
2015-12-22 10:43:48 +0700

    #23 reservation model spec

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
 
 describe Reservation do
+  it { should belong_to(:user) }
+  it { should belong_to(:room) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,13 +10,16 @@ describe User do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
   it { should validate_length_of(:password).is_at_least(8) }
+  it { should have_many(:rooms) }
+  it { should have_many(:reservations) }
+  it { should have_many(:messages) }
 
   context "when duplicated email" do
     it "raises a duplicate error" do
       FactoryGirl.create(:user, email: "tang@mortgageclub.co")
       temp_user = FactoryGirl.build(:user, email: "tang@mortgageclub.co")
       temp_user.valid?
-      expect(tempUser.errors[:email]).to include("has already been taken")
+      expect(temp_user.errors[:email]).to include("has already been taken")
     end
   end
 end
