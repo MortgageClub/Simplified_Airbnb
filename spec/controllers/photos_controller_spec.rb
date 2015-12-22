8142e0f59d30a868b27daa3675f796aa20b71ee7
TangNV <nvtang.khtn@gmail.com>
2015-12-22 16:36:45 +0700

    #23 photo controller spec

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe PhotosController do
+  describe "DELETE #destroy" do
+    let(:room) { FactoryGirl.create(:room) }
+    let!(:photo) { FactoryGirl.create(:photo, image: "https://robohash.org/sitsequiquia.png", room: room) }
+
+    it "deletes the photo" do
+      expect{
+        delete :destroy, id: photo, format: "js"
+      }.to change(Photo, :count).by(-1)
+    end
+
+    it "assigns the requested photos to @photos" do
+      delete :destroy, id: photo, format: "js"
+      expect(assigns(:photos).size).to eq 0
+    end
+
+    it "renders the :destroy js template" do
+      delete :destroy, id: photo, format: "js"
+      expect(response).to render_template("destroy", format: "js")
+    end
+  end
 end
